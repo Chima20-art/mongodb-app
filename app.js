444b2cadecd91164fb2f707f52c30acdbf9b2516
chaimae michich <michichchaimae@gmail.com>
2023-10-24 11:02:47 +0100

    added cors origin 3003

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "http://localhost:3003",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
@@ -19,9 +19,8 @@ mongoose.connect(dbURI, {
 
 const db = mongoose.connection;
 
-const routes = require("./routes/routes");
 const UserConsent = require("./models/UserConsent");
-app.use("/", routes);
+
 app.use(express.json());
 
 // Replace with your MongoDB connection string
