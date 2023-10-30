7829e373386f275ac016da8f8a956714586db091
chaimae michich <michichchaimae@gmail.com>
2023-10-30 15:47:33 +0100

    fix cors

--- api/index.js
+++ api/index.js
@@ -5,8 +5,7 @@ const { v4 } = require("uuid");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const corsOptions = {
-  origin:
-    "https://mongodb-app-6rhb-9nfcpcobu-chima20-art.vercel.app/api/getAll",
+  origin: "http://localhost:3001",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
