3d57d7b74425d4cf31c508acd471c7734cfa8d11
chaimae michich <michichchaimae@gmail.com>
2023-10-30 16:01:23 +0100

    added origin *

--- api/index.js
+++ api/index.js
@@ -5,8 +5,8 @@ const { v4 } = require("uuid");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
+  origin: "*",
+  //credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
