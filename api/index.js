f2cfb1354013d99a35aa6ff11320e10a770cf100
chaimae michich <michichchaimae@gmail.com>
2023-10-30 14:31:24 +0100

    fix

---
+++ api/index.js
@@ -0,0 +1,51 @@
+const app = require("express")();
+const { v4 } = require("uuid");
+const UserConsent = require("../models/UserConsent");
+
+app.get("/api", (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+db.on("error", (err) => {
+  console.error("MongoDB connection error:", err);
+});
+
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my MongoDB app!");
+});
+
+app.get("/getAll", async (req, res) => {
+  try {
+    const data = await UserConsent.find();
+    res.json(data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.post("/", async (req, res) => {
+  console.log("body ", req);
+
+  try {
+    const data = new UserConsent({
+      consentId: req?.body?.consentId,
+      acceptType: req.body.acceptType,
+      acceptedCategories: req.body.acceptedCategories,
+      rejectedCategories: req.body.rejectedCategories,
+    });
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = app;
