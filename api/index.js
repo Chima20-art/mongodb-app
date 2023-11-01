ccc9913c22b266d3b773734298af66b27dbdcbce
chaimae michich <michichchaimae@gmail.com>
2023-11-01 14:13:18 +0100

    removed cors

--- api/index.js
+++ api/index.js
@@ -4,15 +4,15 @@ require("dotenv").config();
 
 const { v4 } = require("uuid");
 const mongoose = require("mongoose");
-const cors = require("cors");
+//const cors = require("cors");
 
-const corsOptions = {
-  origin: "*",
+// const corsOptions = {
+//   origin: "*",
 
-  //credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+//   //credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
+// app.use(cors(corsOptions));
 
 app.use(express.json());
 
