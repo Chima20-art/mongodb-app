f0287b3cc1cf8a5e6db36872e19f7e27a122677f
chaimae michich <michichchaimae@gmail.com>
2023-11-01 15:30:50 +0100

    setHeader for login

--- api/index.js
+++ api/index.js
@@ -90,6 +90,18 @@ app.post("/api/addLog", async (req, res) => {
 
 app.post("/api/login", async (req, res) => {
   console.log("body ", req);
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
 
   let users = [
     {
