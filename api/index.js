be747304c59fb682611c7034fa3ec5fc70fee974
chaimae michich <michichchaimae@gmail.com>
2023-11-01 15:48:46 +0100

    fix

--- api/index.js
+++ api/index.js
@@ -95,8 +95,6 @@ app.post("/api/login", async (req, res) => {
   console.log("body ", req);
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
